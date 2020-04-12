140834cf6c3f1d31b28eb121f720c39c682d8387
ainzzorl <ainzzorl@gmail.com>
2020-04-12 14:55:46 -0700

    Add demonization, implement naive start and stop commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+extern crate daemonize;
+extern crate sysinfo;
+
+use std::fs;
+use std::fs::File;
+use std::{thread, time};
+use std::process;
+use nix::sys::signal::Signal;
 use clap::{App, SubCommand};
+use sysinfo::SystemExt;
+
+
+use daemonize::Daemonize;
 
 fn main() -> () {
+    let pid_opt = read_pid();
+
     let matches = App::new("rustorcli")
                     .version("0.1")
                     .author("ainzzorl <ainzzorl@gmail.com>")
@@ -21,9 +35,11 @@ fn main() -> () {
     match matches.subcommand() {
         ("start", _) => {
             println!("Start subcommand!");
+            start(pid_opt);
         },
         ("stop", _) => {
             println!("Stop subcommand!");
+            stop(pid_opt);
         },
         ("list", _) => {
             println!("List subcommand!");
@@ -36,6 +52,86 @@ fn main() -> () {
         },
         _ => {
             println!("Unknown subcommand!");
+        }
+    }
+}
+
+fn start(pid_opt: Option<i32>) {
+    match pid_opt {
+        Some(pid) => {
+            let sys = sysinfo::System::new_all();
+            let process = sys.get_process(pid);
+            match process {
+                Some(_) => {
+                    println!("Process is already running!");
+                    process::exit(1);
+                },
+                None => {
+                    println!("Process not found");
+                }
+            }
+        },
+        None => {
+            println!("Pid not present");
+        }
+    }
+    let stdout = File::create("/tmp/rustorcli.out").unwrap();
+    let stderr = File::create("/tmp/rustorcli.err").unwrap();
+
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/rustorcli.pid") // Every method except `new` and `start`
+        .working_directory("/tmp")      // for default behaviour.
+        .umask(0o777)                   // Set umask, `0o027` by default.
+        .stdout(stdout)                 // Redirect stdout to `/tmp/daemon.out`.
+        .stderr(stderr)                 // Redirect stderr to `/tmp/daemon.err`.
+        .privileged_action(|| "Executed before drop privileges");
+
+    match daemonize.start() {
+        Ok(_) => run(),
+        Err(e) => eprintln!("Error, {}", e),
+    }
+}
+
+fn run() {
+    loop {
+        println!("Kinda running");
+        thread::sleep(time::Duration::from_secs(10));
+    }
+}
+
+fn stop(pid_opt: Option<i32>) {
+    match pid_opt {
+        Some(pid) => {
+            println!("Pid is present");
+            let sys = sysinfo::System::new_all();
+            let process = sys.get_process(pid);
+            match process {
+                Some(_) => {
+                    println!("Process is running - stopping...");
+                    let pd = nix::unistd::Pid::from_raw(pid);
+                    nix::sys::signal::kill(pd, Signal::SIGINT).unwrap();
+                    println!("Successfully stopped!");
+                },
+                None => {
+                    println!("Process is not running");
+                }
+            }
+        },
+        None => {
+            println!("No pid - do nothing");
+        }
+    }
+}
+
+fn read_pid() -> Option<i32> {
+    let content_opt = fs::read_to_string("/tmp/rustorcli.pid");
+    match content_opt {
+        Ok(content) => {
+            println!("Pid: {}", content);
+            return Some(content.parse().unwrap());
+        }
+        Err(_) => {
+            return None;
         }
     }
 }
