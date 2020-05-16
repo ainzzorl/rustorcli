3ec8c3b35fb301cd3373cd816564b1453f14f745
ainzzorl <ainzzorl@gmail.com>
2020-05-16 14:00:41 -0700

    Configure different config dirs on macos

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,8 @@
-// TODO: what if not on mac?
+#[cfg(target_os = "macos")]
 static CONFIG_DIR: &str = "Library/Application Support/rustorcli";
+
+#[cfg(not(target_os = "macos"))]
+static CONFIG_DIR: &str = ".rustorcli";
 
 pub fn config_directory() -> String {
     let home = dirs::home_dir().unwrap();
