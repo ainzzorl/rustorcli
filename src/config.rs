a17d6c9665dfec8912b50c64d3b001b10cf8eb72
ainzzorl <ainzzorl@gmail.com>
2020-05-23 17:09:42 -0700

    Increase reconnect interval

--- src/config.rs
+++ src/config.rs
@@ -7,7 +7,7 @@ pub const MAX_OUTSTANDING_REQUESTS_PER_PEER: i32 = 10;
 pub const MAX_REQUESTS_PER_TICK_PER_DOWNLOAD: usize = 100;
 
 pub const MAX_PEER_RECONNECT_ATTEMPTS: u32 = 32;
-pub const MIN_PEER_RECONNECT_INTERVAL: Duration = Duration::from_secs(5);
+pub const MIN_PEER_RECONNECT_INTERVAL: Duration = Duration::from_secs(60);
 
 pub const BLOCK_SIZE: u64 = 16384;
 
